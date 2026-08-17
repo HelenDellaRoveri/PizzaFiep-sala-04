025a515a4d25238f872373f7eddb048be3c7b5df
Helen Yohanna Ventura Della Roveri <86626344+HelenDellaRoveri@users.noreply.github.com>
2026-08-17 12:41:14 -0300

    Add files via upload

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function Header(){
+    const [menuAtivo, setMenuAtivo] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuAtivo(!menuAtivo);
+    };
+
+    return(
+        <>
+            <header className="header">
+                <h1 className="logo">Pizza Fiep</h1>
+                {/* QUando clicar, chama a função toggleMenu */}
+                <div className="menu-toggle" onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </header>
+        </>
+    )
+}
